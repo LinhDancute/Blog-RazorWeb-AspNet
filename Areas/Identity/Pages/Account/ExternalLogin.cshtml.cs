da6458c0a9222d926009d4acda1529d64a57e124
LinhDancute <dannguyen11003@gmail.com>
2023-08-16 21:24:20 +0700

    Your commit message here

--- Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -89,33 +89,86 @@ namespace RazorWebAspNet.Areas.Identity.Pages.Account
         
         public IActionResult OnGet() => RedirectToPage("./Login");
 
-        public IActionResult OnPost(string provider, string returnUrl = null)
+        // Post yêu cầu login bằng dịch vụ ngoài
+        // Provider = Google, Facebook ... 
+        // public async Task<IActionResult> OnPostAsync(string provider, string returnUrl = null)
+        // {
+        //     // Kiểm tra yêu cầu dịch vụ provider tồn tại
+        //     var listprovider = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        //     var provider_process = listprovider.Find((m) => m.Name == provider);
+        //     if (provider_process == null)
+        //     {
+        //         return NotFound("Dịch vụ không chính xác: " + provider);
+        //     }
+
+        //     // redirectUrl - là Url sẽ chuyển hướng đến - sau khi CallbackPath (/dang-nhap-tu-google) thi hành xong
+        //     // nó bằng identity/account/externallogin?handler=Callback 
+        //     // tức là gọi OnGetCallbackAsync 
+        //     var redirectUrl = Url.Page("./su-dung-dich-vu-ngoai/", pageHandler: "Callback", values: new { returnUrl });
+
+        //     // Cấu hình 
+        //     var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+
+        //     // Chuyển hướng đến dịch vụ ngoài (Googe, Facebook)
+        //     //return new ChallengeResult(provider, properties);
+        //     return Redirect(redirectUrl);
+        // }
+
+        public async Task<IActionResult> OnPostAsync(string provider, string returnUrl = null)
         {
-            // Request a redirect to the external login provider.
+            // Kiểm tra yêu cầu dịch vụ provider tồn tại
+            var listprovider = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var provider_process = listprovider.Find((m) => m.Name == provider);
+            if (provider_process == null)
+            {
+                return NotFound("Dịch vụ không chính xác: " + provider);
+            }
+
+            // Set the returnUrl as a query parameter for the ExternalLogin page
             var redirectUrl = Url.Page("./ExternalLogin", pageHandler: "Callback", values: new { returnUrl });
-            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
-            return new ChallengeResult(provider, properties);
+
+            // Check if the redirectUrl is null or empty before performing the redirect
+            if (!string.IsNullOrEmpty(redirectUrl))
+            {
+                // Redirect to the ExternalLogin page
+                return Redirect(redirectUrl);
+            }
+            else
+            {
+                // Handle the case when redirectUrl is null or empty
+                // You can return an appropriate view or message
+                return NotFound("Invalid redirect URL.");
+            }
         }
+
+
 
         public async Task<IActionResult> OnGetCallbackAsync(string returnUrl = null, string remoteError = null)
         {
+            
+            //_logger.LogInformation("Callback received. RemoteError: {remoteError}", remoteError);
             returnUrl = returnUrl ?? Url.Content("~/");
             if (remoteError != null)
             {
-                ErrorMessage = $"Error from external provider: {remoteError}";
-                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
-            }
-            var info = await _signInManager.GetExternalLoginInfoAsync();
-            if (info == null)
-            {
-                ErrorMessage = "Error loading external login information.";
+                ErrorMessage = $"Lỗi từ dịch vụ ngoài: {remoteError}";
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
 
-            // Sign in the user with this external login provider if the user already has a login.
+            // Lấy thông tin do dịch vụ ngoài chuyển đến
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                ErrorMessage = "Không lấy được thông tin từ dịch vụ ngoài.";
+                return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+            }
+
+            // Đăng nhập bằng thông tin LoginProvider, ProviderKey từ info cung cấp bởi dịch vụ ngoài
+            // User nào có 2 thông tin này sẽ được đăng nhập - thông tin này lưu tại bảng UserLogins của Database
+            // Trường LoginProvider và ProviderKey ---> tương ứng UserId 
             var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
             if (result.Succeeded)
             {
+                // User đăng nhập thành công vào hệ thống theo thông tin info
                 _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
                 return LocalRedirect(returnUrl);
             }
@@ -125,11 +178,22 @@ namespace RazorWebAspNet.Areas.Identity.Pages.Account
             }
             else
             {
-                // If the user does not have an account, then ask the user to create an account.
+                var userExisted = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+                if (userExisted != null)
+                {
+                    // Đã có Acount, đã liên kết với tài khoản ngoài - nhưng không đăng nhập được
+                    // có thể do chưa kích hoạt email
+                    return RedirectToPage("./RegisterConfirmation", new { Email = userExisted.Email });
+
+                }
+
+                // Chưa có Account liên kết với tài khoản ngoài
+                // Hiện thị form để thực hiện bước tiếp theo ở OnPostConfirmationAsync
                 ReturnUrl = returnUrl;
                 ProviderDisplayName = info.ProviderDisplayName;
                 if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
                 {
+                    // Có thông tin về email từ info, lấy email này hiện thị ở Form
                     Input = new InputModel
                     {
                         Email = info.Principal.FindFirstValue(ClaimTypes.Email)
@@ -146,12 +210,56 @@ namespace RazorWebAspNet.Areas.Identity.Pages.Account
             var info = await _signInManager.GetExternalLoginInfoAsync();
             if (info == null)
             {
-                ErrorMessage = "Error loading external login information during confirmation.";
+                ErrorMessage = "Lỗi lấy thông tin từ dịch vụ ngoài.";
                 return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
             }
 
             if (ModelState.IsValid)
             {
+
+                //Input.Email
+                var registeredUser = await _userManager.FindByEmailAsync(Input.Email);
+                string externalEmail = null;
+                AppUser externalEmailUser = null;
+                //Claim ~ Dac tinh mo ta 1 doi tuong
+                if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
+                {
+                    externalEmail = info.Principal.FindFirstValue(ClaimTypes.Email);
+                }
+
+                if (externalEmail != null)
+                {
+                    externalEmailUser = await _userManager.FindByEmailAsync(externalEmail);
+                }
+
+                if ((registeredUser != null) && (externalEmailUser != null))
+                {
+                    // externalEmail == Input.Email
+                    if (registeredUser.Id == externalEmailUser.Id)
+                    {
+                        // Lien ket tai khoan, dang nhap
+                        var resultLink = await _userManager.AddLoginAsync(registeredUser, info);
+                        if (resultLink.Succeeded)
+                        {
+                            await _signInManager.SignInAsync(registeredUser, isPersistent: false);
+                            return LocalRedirect(returnUrl);
+                        }
+                    }
+                    else
+                    {
+                        // registeredUser = externalEmailUser (externalEmail != Input.Email)
+                        /*
+                        info => user1 (mail1@abc.com)
+                             => user2 (mail2@abc.com)
+                        */
+                        ModelState.AddModelError(string.Empty, "Không liên kết được tài khoản, hãy sử dụng email khác");
+                        //return Page();
+                    }
+                }
+
+
+                return Content("Dừng lại ở đây");
+
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
